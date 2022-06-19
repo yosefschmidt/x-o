67ac7379e6df6a01f53e6be6d0ed3d69bfb5c9c5
yosefschmidt <102920177+yosefschmidt@users.noreply.github.com>
2022-06-19 10:40:02 +0300

    Update clom.js

--- clom.js
+++ clom.js
@@ -1,52 +1,186 @@
-let sumcards=prompt("ghf")
-let countsumcards=0
-const cardsw=[]
-let countsteps=0
-let creatdiv= document.createElement("div")
-eix="www.google.com/url?sa=i&url=https%3A%2F%2Fwww.emojiall.com%2Fhe%2Femoji%2F%25E2%259D%258C&psig=AOvVaw2YC5c3yb6DBtzv5h0Do80C&ust=1652357588598000&source=images&cd=vfe&ved=2ahUKEwiUuZPqtdf3AhWRQEEAHbnmBwEQjRx6BAgAEAs"
-eigol= "www.google.com/url?sa=i&url=https%3A%2F%2Fhe-il.facebook.com%2Fsholefet%2Fposts%2F%25D7%2599%25D7%25A6%25D7%25A8%25D7%25AA%25D7%2599-%25D7%25A2%25D7%2599%25D7%2592%25D7%2595%25D7%259C-%25D7%259B%25D7%2596%25D7%2594-%25D7%2591-powerpoint-%25D7%2590%25D7%25AA%25D7%259E%25D7%2595%25D7%259C-%25D7%259B%25D7%2593%25D7%2599-%25D7%259C%25D7%25A1%25D7%259E%25D7%259F-%25D7%25AA%25D7%259E%25D7%2595%25D7%25A0%25D7%2594-%25D7%25A2%25D7%259D-%25D7%259E%25D7%25A1%25D7%25A4%25D7%25A8-%25D7%2590%25D7%2591%25D7%259C-%25D7%259E%25D7%25A8%25D7%259B%25D7%2596-%25D7%2594%25D7%25A1%25D7%2599%25D7%2595%25D7%25A2-%25D7%259C%25D7%25A0%25D7%25A4%25D7%2592%25D7%25A2%25D7%2595%25D7%25AA%2F2346373775581917%2F&psig=AOvVaw1FosDwG1cg2dEUV6V2usRd&ust=1652357460630000&source=images&cd=vfe&ved=2ahUKEwjS95Cttdf3AhWLTMAKHT2gBU4QjRx6BAgAEAs"
 
-for(v = 0; v < sumcards; v++){
+let countsumcards = 0;
+const cardopen = [];
+let countsteps = 0;
+const users = [
+  { name: "yosef", winers: 0, points: 0 },
+  { name: "moria", winers: 0, points: 0 },
+];
+let steps=[]
+let creatdiv = document.createElement("div");
+
+clickstart=document.getElementById("submit")
+
+clickstart.addEventListener('click',()=>{
+  let sumcards=document.getElementById("numstart").value
+
+  repeatcard = "auto ".repeat(sumcards);
+document.getElementById("board").style.gridTemplateColumns = repeatcard;
+  
+  for (v = 0; v < sumcards; v++) {
     // debugger
-    cardsw[v]=[]
-    for(i = 0; i < sumcards; i++){
-       a=creatdiv
-
-    cardsw[v][i]={div:a,id:i,class:"notclass"}
+    cardopen[v] = [];
+    for (i = 0; i < sumcards; i++) {
+      // debugger
+      let a = [];
+      a[i] = document.createElement("div");
+      a[i].className = "board";
+      id = i;
+      countsumcards++;
+      b = a[i];
+      b.id = countsumcards;
+      cardopen[v][i] = { div: b, id: countsumcards };
     }
-    }
-console.log(cardsw);
+  }
 
+  
+  creatdiv = document.createElement("div");
+  for (i in cardopen) {
+    cardopen[i].forEach(creatgame);
+  }
+document.getElementById("submit").style.display = "none";
+document.getElementById("numstart").style.display = "none";
+document.getElementById("inpname0").style.display = "none";
+document.getElementById("inpname1").style.display = "none";
+
+repeatcard = ` ${users.length}fr `.repeat(users.length);
+document.getElementById("playrs").style.gridTemplateColumns = repeatcard;
+repeatcard = ` ${users.length}fr `.repeat(users.length);
+document.getElementById("playrs").style.gridTemplateColumns = repeatcard;
+document.getElementById("name0").innerHTML = textuser(0);
+document.getElementById("name1").innerHTML = textuser(1);
+// document.getElementById("startgame").style="display:nune;"
+})
+function textuser(e) {
+  return `${users[e].name}<br/>winn'S :${users[e].winers}`;
+}
+
+  let sumcards=document.getElementById("numstart").value
+
+
+console.log(cardopen);
 
 const board = document.getElementById("board");
+
 function creatgame(v, i, arr) {
-    
-    const element = document.createElement("div");
-    // element.innerHTML ="y";
-    
-    element.id =countsumcards++;
-    element.className = "board";
-    return document.getElementById("board").appendChild(element)
-    
+  console.log(v.div.id + "yos");
+  // debugger
+
+  const element = document.createElement("div");
+
+  // v.div.id =v.id;
+  // v.div.className = "board";
+  return document.getElementById("board").appendChild(v.div);
+  // return document.getElementById("board").appendChild(element)
 }
 
+board.addEventListener("click", (i) => {
+  imgX(i),
+  setTimeout(() => {
+     testwinner3(cardopen,XO = "X")
+     ,testwinner3(cardopen,XO = "O");
+  }, 1);
+});
 
-board.addEventListener("click",(i)=>{imgX(i)})
-function imgX(i){
-        console.log();
-  const immg=document.createElement("img")
-    immg.src=(countsteps%2==0)?("https://img.rami-levy.co.il/product/7290112191878/small.jpg"):("https://img.rami-levy.co.il/product/7290002053088/small.jpg");
-    immg.id="toimg"
-    countsteps++
+function imgX(i) {
+  if (i.target.className == "board") {
+  steps.push(i.target)
+
+    //    imgxo=(countsteps%2==0)?("x.png"):("./y.png");
+    console.log(1);
+    const immg = document.createElement("img");
+    immg.src = countsteps % 2 == 0 ? "./x.png" : "./y.png";
+    i.target.className += countsteps % 2 == 0 ? " X" : " O";
+    immg.id = "toimg";
+    countsteps++;
     console.log(i.target);
-    return document.getElementById(i.target.id).appendChild(immg)
+    // return i.target.style=`background-image: url(${imgxo});`
+
+    return document.getElementById(i.target.id).appendChild(immg);
+  }
+}
+
+function testwinner3(cardopen, XO) {
+  
+  for (i of cardopen) {
+    let isXO = 0;
+    for (v of i) {
+       if(XO=="O") {debugger}
+      // console.log((v.div.classList.value.includes(XO)));
+      v.div.classList.value.includes(XO) ? isXO++ : (isXO = 0);
+      (isXO >= 3)? alert("winner!!!"):null
+
+    }}
+  for(i=0; i < sumcards; i++){
+    // debugger
+    let isXO = 0;
+    for (v=0; v < sumcards; v++) {
+      // console.log((v.div.classList.value.includes(XO)));
+      cardopen[v][i].div.classList.value.includes(XO) ? isXO++ : (isXO = 0);
+      (isXO >= 3)? alert("winner!!!up"):null
     }
 
+  }
+  for(i = 1; i+2 <= sumcards; i++){
+    let isXO = 0;
+    for(v = 0; v <sumcards; v++){
+      document.getElementById(i+v*(Number(sumcards)+1)).classList.value.includes(XO) ? isXO++ : (isXO = 0);
+      (isXO >= 3)? alert("winner!!!\/\1\/\/"):null
+    }
+  }
 
-for(i in cardsw){
-    forid=i
-    cardsw[i].forEach(creatgame)
+  for(i = Number(sumcards); i <=3; i--){
+    // console.log(i+" "+XO);
+    let isXO = 0;
+    for(v =0; v <sumcards; v++){
+      // if(XO=="X") {debugger}
+      as=i-v*2
+      // console.log(i+v*(Number(sumcards)-1));
+      document.getElementById(i+v*(Number(sumcards)-1)).classList.value.includes(XO) ? isXO++ : (isXO = 0);
+      // console.log(document.getElementById(i+v*sumcards+1).classList.value.includes(XO));
+      (isXO >= 3)? alert("winner!!!\/\ghtrt\/\/"):null
+    }
+  }
+  
+  
+  
+  
+  
+  }
+  function ids(num,plus){
+for(i=0;i<num;i++){
+
 }
+  }
 
-repeatcard= "auto ".repeat(sumcards)
-document.getElementById("board").style.gridTemplateColumns=repeatcard;
+  document.getElementById("back").addEventListener('click',()=>{
+   let  a=steps.pop()
+a.removeChild(a.firstChild)
+a.classList.remove(a.classList[1])
+countsteps--
+  })
+  
+  document.getElementById("reset").addEventListener('click',()=>{
+for(i=0;steps.length>0;i){
+  let  a=steps.pop()
+  a.removeChild(a.firstChild)
+  a.classList.remove(a.classList[1])
+  countsteps--
+}
+  })
+
+  document.getElementById("homepage").addEventListener('click',()=>{
+    document.getElementById("submit").style.display = "revert";
+    document.getElementById("numstart").style.display = "revert";
+    document.getElementById("board").replaceChildren("")
+    document.getElementById("name0").remove()
+document.getElementById("name1").remove()
+    document.getElementById("inpname0").style.display = "revert";
+document.getElementById("inpname1").style.display = "revert";
+
+  })
+  
+
+
+repeatcard = "auto ".repeat(sumcards);
+document.getElementById("board").style.gridTemplateColumns = repeatcard;
